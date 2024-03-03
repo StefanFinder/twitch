5fd2f8fb3d4a4a9df780ea6d58743169969a72e3
Qiwei Chen <qiweic@usc.edu>
2024-03-02 16:43:44 -0800

    Twitch API

--- src/main/java/com/laioffer/twitch/hello/Address.java
+++ src/main/java/com/laioffer/twitch/hello/Address.java
@@ -1,5 +1,7 @@
 package com.laioffer.twitch.hello;
 
+// by default help us create all private fields
+// override HashCode, toString and getter.
 public record Address(
         String street,
         String city,
